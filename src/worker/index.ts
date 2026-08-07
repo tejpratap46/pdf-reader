0fcbcd61df068064e70509316690bcda7ade7ab6
Tej Pratap Singh <tejpratap46@gmail.com>
2026-08-07 03:31:16 +0000

    fix: update pwa manifest

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,5 +1,9 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+
 const app = new Hono<{ Bindings: Env }>();
+
+app.use("*", cors());
 
 app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
 
